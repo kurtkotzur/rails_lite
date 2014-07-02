913369f83e3e791d73143ef56e9c659d0572560f
Kurt Kotzur <kgk1@rice.edu>
2014-07-02 13:33:04 -0700

    Update session.rb

--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -2,8 +2,6 @@ require 'json'
 require 'webrick'
 
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
   def initialize(req)
     cookies = req.cookies
     @session_hash = {}
@@ -22,8 +20,6 @@ class Session
     @session_hash[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
     res.cookies << WEBrick::Cookie.new("_rails_lite_app", @session_hash.to_json)
   end
